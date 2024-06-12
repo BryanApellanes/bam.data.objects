cd6a7e941f0230073875d8903de01fe10e9527f9
Bryan Apellanes <bryan@threeheadz.com>
2024-06-12 13:01:04 -0500

    net8

--- bam.data.objects/ObjectKey.cs
+++ bam.data.objects/ObjectKey.cs
@@ -7,7 +7,9 @@ public class ObjectKey : IObjectKey
 {
     public TypeDescriptor Type { get; set; }
     public IStorageIdentifier StorageIdentifier { get; internal set; }
+    
     public string Id { get; internal set; }
+    
     public string Key { get; internal set; }
 
     public override string ToString()
