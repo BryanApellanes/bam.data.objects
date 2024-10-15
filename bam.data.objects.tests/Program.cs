f1cd57841e2cd08a17171b2d89e2676e91792708
Bryan Apellanes <bryan@threeheadz.com>
2024-10-15 09:35:48 -0500

    wip

--- bam.data.objects.tests/Program.cs
+++ bam.data.objects.tests/Program.cs
@@ -19,6 +19,7 @@ namespace Bam.Application
                 MenuManager = MenuManager.FromOptions(options)
             };
             BamConsoleContext.Current.Main(args);
+            
         }
     }
 }
